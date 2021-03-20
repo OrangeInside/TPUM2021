80f93821d83fea082c154c603a9f9b5d686dd23c
Zawisz <zawisza_mateusz@wp.pl>
2021-03-20 17:47:49 +0100

    Update for test program
    One additional console write line added

--- TestProject/TestProject/Program.cs
+++ TestProject/TestProject/Program.cs
@@ -7,6 +7,8 @@ namespace TestProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("This line was added by me. Can you see this?");
         }
     }
 }
