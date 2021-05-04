5c4d7360bb73c9c984a46fe29649f955460783c6
B.Krukowski <38346579+OrangeInside@users.noreply.github.com>
2021-05-04 18:48:54 +0200

    DataFactory fixed

--- HelveteShop/Data/DataFactory.cs
+++ HelveteShop/Data/DataFactory.cs
@@ -10,25 +10,20 @@ namespace Data
         {
             DataContext data = new DataContext();
 
-            Client c1 = new Client(0, "Odo", "Milorgveien 121");
-            Client c2 = new Client(4, "DarkLordOfChaos666", "|m|> Hell <|m|");
-            data.clients.Add(c1);
+            data.clients.Add(new Client(0, "Odo", "Milorgveien 121"));
             data.clients.Add(new Client(1, "Varg", "Grenaderveien 128"));
             data.clients.Add(new Client(2, "Kjetil", "Camlann Place"));
             data.clients.Add(new Client(3, "Andrew", "Tappenhaugen 124"));
-            data.clients.Add(c2);
-
-            Vinyl v1 = new Vinyl(0, "Filosofem", "Burzum", 5.99M);
-            Vinyl v2 = new Vinyl(3, "Black Metal", "Venom", 2.99M);
-            data.vinyls.Add(v1);
+            data.vinyls.Add(new Vinyl(0, "Filosofem", "Burzum", 5.99M));
             data.vinyls.Add(new Vinyl(1, "Transylvanian Hunger", "Darkthrone", 3.99M));
             data.vinyls.Add(new Vinyl(2, "De Mysteriis Dom Sathanas", "Mayhem", 4.99M));
-            data.vinyls.Add(v2);
+            data.vinyls.Add(new Vinyl(3, "Black Metal", "Venom", 2.99M));
             data.vinyls.Add(new Vinyl(4, "With Hearts Toward None", "Mgla", 1.99M));
 
-            data.orders.Add(new Order(0, c1, v1));
-            data.orders.Add(new Order(1, c2, v2));
-
+            data.orders.Add(new Order(0, data.clients[0], data.vinyls[0]));;
+            data.orders.Add(new Order(1, data.clients[1], data.vinyls[1]));;
+            data.orders.Add(new Order(2, data.clients[2], data.vinyls[2]));;
+          
             return data;
         }
     }
