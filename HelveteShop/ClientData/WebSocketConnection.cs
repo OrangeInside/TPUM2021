519bb69fe1ccc87d6597f53aeb6e1f7df87ac873
B.Krukowski <38346579+OrangeInside@users.noreply.github.com>
2021-05-22 17:36:14 +0200

    Working on web sockets

---
+++ HelveteShop/ClientData/WebSocketConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+namespace ClientData
+{
+    public abstract class WebSocketConnection
+    {
+        public virtual Action<string> onMessage { set; protected get; } = x => { };
+        public virtual Action onClose { set; protected get; } = () => { };
+        public virtual Action onError { set; protected get; } = () => { };
+
+        protected abstract Task SendTask(string message);
+
+        public async Task AsyncSend(string message)
+        {
+            await SendTask(message);
+        }
+
+        public abstract Task AsyncDisconnect();
+
+
+    }
+}
